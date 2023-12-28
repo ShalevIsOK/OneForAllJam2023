4b9b8ea99c01bc18784f566e6c1c95e2327e1adf
ShalevIsOK <shalevbabai@gmail.com>
2023-12-28 22:42:39 +0200

    Better Input (Shalev is stupid)

--- Assets/KeyboardMovementInput.cs
+++ Assets/KeyboardMovementInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,76 +9,107 @@ public class KeyboardMovementInput : MonoBehaviour
     private MoveStates moveType;
     [SerializeField] private float speed = 1f;
     [SerializeField] private PlayerInput playerInput;
-    private Rigidbody2D rigidbody2;
+    private Rigidbody2D rb2d;
+
+    private bool up = false;
+    private bool down = false;
+    private bool left = false;
+    private bool right = false;
     
 
     private void Start()
     {
-        rigidbody2 = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
+        SetMoveType();
         SetMoveVector();
+
+    }
+
+    private void SetMoveType()
+    {
+        //check current axis first, if none defaults to vertical
+        if (moveType == MoveStates.Up || moveType == MoveStates.Down || moveType == MoveStates.none)
+        {
+            if (up != down)
+            {
+            Debug.Log("1");
+                if (up) { moveType = MoveStates.Up; Debug.Log("up"); return; }
+                else { moveType = MoveStates.Down; return; }
+            }
+            if (left != right)
+            {
+                if (left) { moveType = MoveStates.Left; return; }
+                else { moveType = MoveStates.Right; return; }
+            }
+            
+        }
+        else
+        {
+            if (left != right)
+            {
+                if (left) { moveType = MoveStates.Left; return; }
+                else { moveType = MoveStates.Right; return; }
+            }
+            if (up != down)
+            {
+                if (up) { moveType = MoveStates.Up; return; }
+                else { moveType = MoveStates.Down; return; }
+            }
+        }
     }
 
     private void SetMoveVector()
     {
         switch (moveType)
         {
+            case MoveStates.Left:
+                rb2d.velocity = new Vector2(-speed, 0);
+                if (transform.position.x <= ScreenBounds.LeftEdge) rb2d.velocity = Vector2.zero; 
+                return;
+               
+            case MoveStates.Right:
+                if (transform.position.x >= ScreenBounds.RightEdge) rb2d.velocity = Vector3.zero;
+                else rb2d.velocity = new Vector3(speed, 0);
+                return;
+
             case MoveStates.Up:
-                if(transform.position.y >= ScreenBounds.TopEdge)
-                {
-                    rigidbody2.velocity = Vector2.zero; return;
-                }
-                rigidbody2.velocity = new Vector2(0, speed);
+                rb2d.velocity = new Vector2(0, speed);
+                if (transform.position.y >= ScreenBounds.TopEdge) rb2d.velocity = Vector3.zero;
                 return;
 
             case MoveStates.Down:
-                if (transform.position.y <= ScreenBounds.BottomEdge)
-                {
-                    rigidbody2.velocity = Vector2.zero; return;
-                }
-                rigidbody2.velocity = new Vector2(0, -speed);
+                rb2d.velocity = new Vector2(0, -speed);
+                if (transform.position.y <= ScreenBounds.BottomEdge) rb2d.velocity = Vector3.zero;
                 return;
-
-            case MoveStates.Left:
-                if (transform.position.x <= ScreenBounds.LeftEdge)
-                {
-                    rigidbody2.velocity = Vector2.zero; return;
-                }
-                rigidbody2.velocity = new Vector2(-speed, 0);
-                return;
-
-            case MoveStates.Right:
-                if (transform.position.x >= ScreenBounds.RightEdge)
-                {
-                    rigidbody2.velocity = Vector2.zero; return;
-                }
-                rigidbody2.velocity = new Vector2(speed, 0);
-                return;
-
         }
     }
 
     public void OnUpMovement(InputAction.CallbackContext context)
     {
-        moveType = MoveStates.Up;
+        if (context.started) { up = true; }
+        if (context.canceled) { up = false; }
     }
 
     public void OnDownMovement(InputAction.CallbackContext context)
     {
-        moveType = MoveStates.Down;
+        if (context.started) { down = true; }
+        if (context.canceled) { down = false; }
     }
 
     public void OnLeftMovement(InputAction.CallbackContext context)
     {
-        moveType = MoveStates.Left;
+        if (context.started) { left = true; }
+        if (context.canceled) { left = false; }
     }
 
     public void OnRightMovement(InputAction.CallbackContext context)
     {
-        moveType= MoveStates.Right;
+        if (context.started) { right = true; }
+        if (context.canceled) { right = false; }
     }
 }
 
